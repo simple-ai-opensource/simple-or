b2908012b500245bbe510a6095a048ca379eb0c8
Lennart Damen <lennart.damen@coolblue.nl>
2020-12-10 00:37:30 +0100

    core: update token, add import to __init__

--- simpleor/__init__.py
+++ simpleor/__init__.py
@@ -1,6 +1,9 @@
 """Top-level package for simpleor."""
 import sys
 import logging
+import simpleor.base
+import simpleor.scheduler
+import simpleor.cli
 
 __author__ = """Lennart Damen"""
 __email__ = "lennart.damen@hotmail.com"
@@ -9,7 +12,7 @@ __version__ = "0.0.0"
 logging.basicConfig(
     stream=sys.stdout,
     format="%(asctime)s | %(levelname)-8s | Process: %(process)d | %(name)s:%("
-           "funcName)s:%(lineno)d - %(message)s",
+    "funcName)s:%(lineno)d - %(message)s",
     level=logging.DEBUG,
 )
 
