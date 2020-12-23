560de3fe8c2d68c8601d86ca3a394da098708488
Lennart Damen <lennart.damen@coolblue.nl>
2020-12-23 21:32:08 +0100

    Bump version: 0.0.11 → 0.0.12

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     name="simpleor",
     packages=find_packages(include=["simpleor", "simpleor.*"]),
     url="https://github.com/simple-ai-opensource/simple-or",
-    version="0.0.11",
+    version="0.0.12",
     zip_safe=False,
 )
