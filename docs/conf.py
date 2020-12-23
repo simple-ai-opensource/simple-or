808836ed59c15f7cadf75699a07efe5b155fe0b4
Lennart Damen <lennart.damen@coolblue.nl>
2020-12-23 21:14:59 +0100

    docfix

--- docs/conf.py
+++ docs/conf.py
@@ -19,11 +19,11 @@
 #
 import os
 import sys
+import simpleor
 
 sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, "../simpleor")
 
-import simpleor
 
 # -- General configuration ---------------------------------------------
 
@@ -85,7 +85,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "classic"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
